c483d7d4272d564a55d78e6d78fd9fb1ff2b8b9e
BRD <909765457@qq.com>
2026-09-05 01:00:26 +0800

    day2:first try for cout and cin

---
+++ learncpp/day02/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>  // for std::cout and std::cin
+
+int main()
+{
+    std::cout << "Enter three numbers: "; // ask user for a number
+
+    int x{};       // define variable x to hold user input (and value-initialize it)
+    std::cin >> x; // get number from keyboard and store it in variable x
+    
+	int y{};       // define variable y to hold user input (and value-initialize it)
+    std::cin >> y; // get number from keyboard and store it in variable y
+
+	int z{};       // define variable z to hold user input (and value-initialize it)
+    std::cin >> z; // get number from keyboard and store it in variable z
+
+    std::cout << "You entered " << x << ", " << y << ", and " << z << '\n';
+    return 0;
+}
