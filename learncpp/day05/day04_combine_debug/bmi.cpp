cf6ce15ab495da1b1813e5cd38617ec2262c8203
BRD <909765457@qq.com>
2026-09-09 00:52:07 +0800

    day05_debug

---
+++ learncpp/day05/day04_combine_debug/bmi.cpp
@@ -0,0 +1,6 @@
+﻿#include "bmi.h"    // 也包含头文件：让编译器检查"实现和合同一致"
+
+double calcBmi(double height, double weight)    // 定义：签名 + 函数体
+{
+    return weight / (height * height);
+}
---
+++ learncpp/day05/day04_combine_debug/bmi.h
@@ -0,0 +1,3 @@
+﻿#pragma once    // 防止本文件被重复包含（2.12 刚学的）
+
+double calcBmi(double height, double weight);   // 声明：只有签名，分号结尾，没有函数体
